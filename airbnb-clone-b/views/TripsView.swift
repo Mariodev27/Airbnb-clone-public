44f6908116f1c388c7a44e032d61db566fc891be
Mariodev27 <mariosantisteban23@gmail.com>
2023-11-17 17:50:35 -0500

    Lab11

--- airbnb-clone-b/views/TripsView.swift
+++ airbnb-clone-b/views/TripsView.swift
@@ -2,14 +2,64 @@
 //  TripsView.swift
 //  airbnb-clone-b
 //
-//  Created by Alumno on 10/11/23.
+//  Created by Mariodev27 on 10/11/23.
 //
 
 import SwiftUI
+import MapKit
 
 struct TripsView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -12.0450947, longitude: -76.9545933), span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                HStack {
+                    Image("image")
+                        .resizable()
+                        .frame(width: 120)
+                    VStack(alignment: .leading, spacing:4) {
+                        Text("La Calera, Colombia")
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                            .padding(.leading)
+                        Spacer()
+                        Text("Nov 19 - 24")
+                            .font(.caption)
+                            .fontWeight(.light)
+                            .padding(.leading)
+                        Text("S/ 268 night")
+                            .font(.caption)
+                            .padding(.leading)
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Image(systemName: "star.fill")
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 17, height: 15)
+                            Text("4.91")
+                                .font(.caption)
+                        }
+                    }
+                    .padding(.vertical)
+                    Spacer()
+                    VStack {
+                        Image(systemName: "heart")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .padding(.top, 10)
+                        Spacer()
+                    }
+                    .padding(.trailing)
+                }
+                .frame(height: 120)
+                .background(.white)
+            }
+            .padding()
+        }
     }
 }
 
